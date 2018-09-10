fa486a56b5a49e03c2f1490e33d58e036dc035e0
Dale Halling <hrangar@protonmail.com>
2018-09-10 14:27:42 +0800

    todo list working

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList";
+
+
+var destination = document.getElementById("container");
+
+ReactDOM.render(
+  <div>
+    <TodoList />
+  </div>,
+  destination
+);
